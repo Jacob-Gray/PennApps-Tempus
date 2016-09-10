31c5e43ac6e441469850f51722d57c8ed9400d3e
Jacob Gray <jacob@jacobgray.me>
2016-09-10 12:40:24 -0400

    Updated all the thingz!

--- backend/public/javascripts/app.js
+++ backend/public/javascripts/app.js
@@ -1,24 +1,35 @@
 var page = angular.module('tempus', ['ngRoute', 'ngAnimate']);
 
-page.config(['$routeProvider','$locationProvider',
-    function($routeProvider, $locationProvider) {
-        $routeProvider.
-        when('/', {
-            templateUrl: 'home.html',
-            controller: 'home'
-        }).when('/login', {
-            templateUrl: 'login.html',
-            controller: 'login'
-        }).
-        otherwise({
-            redirectTo: '/'
-        });
-        $locationProvider.html5Mode(true);
-    }
+page.config(['$routeProvider', '$locationProvider',
+	function($routeProvider, $locationProvider) {
+		$routeProvider.
+		when('/', {
+			templateUrl: 'home.html',
+			controller: 'home'
+		}).when('/login', {
+			templateUrl: 'login.html',
+			controller: 'login'
+		}).when('/signup', {
+			templateUrl: 'signup.html',
+			controller: 'signup'
+		}).
+		otherwise({
+			redirectTo: '/'
+		});
+		// $locationProvider.html5Mode(true);
+	}
 ]);
-page.controller('home', function($scope) {
+var setHeader = null;
 
+page.controller('home', function($scope) {
+  $("header p").html("Already have an account?");
+  $("header a.btn").html("Login").attr("href","#login");
 });
 page.controller('login', function($scope) {
-
+  $("header p").html("Don't have an account?");
+  $("header a.btn").html("Sign up").attr("href","#signup");
+});
+page.controller('signup', function($scope) {
+  $("header p").html("Already have an account?");
+  $("header a.btn").html("Login").attr("href","#login");
 });
