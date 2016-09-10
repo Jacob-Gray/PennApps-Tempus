dd2a2c706a1728fa99b0c5e64d4b689572b925e9
Jacob Gray <jacob@jacobgray.me>
2016-09-09 21:15:23 -0400

    Fixed it

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,15 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.get('/users/:name', function (req, res) {
-   res.send('Hello '+ req.params.name + "!");
-})
-
 app.get("/", function(req, res){
     res.send("test")
 })
 
-var server = app.listen(process.env.PORT, function () {
-
-console.log("LISTENING")
+var server = app.listen(3333, function () {
+  console.log(server.address())
 })
